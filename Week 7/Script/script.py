b5fa7ffbbb4dcfeafd3120dee906fa5f5a1242ae
zhichengzhang1995 <zhichengzhang00@umail.ucsb.edu>
2019-02-26 22:40:56 -0800

    Week 8

--- Week 7/Script/script.py
+++ Week 7/Script/script.py
@@ -2,35 +2,37 @@ import os
 import psutil
 import subprocess
 import time
-
 from memory_profiler import profile
-@profile
-def s():
-	mem_use = 0
-	t = time.time()
-	PERCPU_start=psutil.cpu_times(percpu=True)
 
-	#p = subprocess.Popen('pypy 1.py 100', shell = True, creationflags =0)
-	p = subprocess.Popen('python 3.py 100', shell = True, creationflags =0)
-	pt = subprocess.Popen('time -p python 3.py 100', shell = True, creationflags =0)
-	while p.poll() is None:
+#@profile
+def test_in_python():
+	sub_thread_time = subprocess.Popen("/usr/bin/time -lp pypy fibonacci3.py 100", shell = True, creationflags =0)
+	#Mem info
+	memory_useinfo = 0
+	#Basic time
+	start_time = time.time()
+	#Thread1:
+	sub_thread = subprocess.Popen('pypy fibonacci3.py 100', shell = True, creationflags = 0)
+	#Log cpu_time, memory_useinfo
+	while sub_thread.poll() is None:
 		try:
-			cpu_time = psutil.Process(p.pid).cpu_times()
-			mem_use = max(psutil.Process(p.pid).memory_info().rss, mem_use)
+			cpu_time = psutil.Process(sub_thread.pid).cpu_times()
+			memory_useinfo = max(psutil.Process(sub_thread.pid).memory_info().rss, memory_useinfo)
 		except:
 			break
+	#Info
+	print 'System time: ', cpu_time.system
+	print 'User time: ', cpu_time.user
 
 	print 'CPU time: ', cpu_time.user + cpu_time.system
-	print 'memory usage: ', str(round(mem_use / 1024, 0)) + ' KB'
-	print 'Elapsed time: ', time.time() - t
-
-	PERCPU_exit=psutil.cpu_times(percpu = True)
-	CPU_load = []
-	for i in range(psutil.cpu_count()):
-		CPU_load.append(((PERCPU_exit[i].user - PERCPU_start[i].user) / (PERCPU_exit[i].user -
-	    PERCPU_start[i].user + PERCPU_exit[i].idle - PERCPU_start[i].idle)) * 100 // 1)
-	print 'CPU_load: ', CPU_load
+	print 'Real time: ', time.time() - start_time
+	print 'Memory usage: ', memory_useinfo, 'Byte'
+	#print 'Memory usage: ', str(round(memory_useinfo / 1024, 0)), 'KB'
+  
+	#Thread2:
+	#sub_thread_time = subprocess.Popen("time TIMEFORMAT=$'Real_time %2R User_time %2U System_time %2S' python fibonacci3.py 100", shell = True, creationflags =0)
+	#sub_thread_time = subprocess.Popen("/usr/bin/time -lp python fibonacci3.py 100", shell = True, creationflags =0)
 
 
 if __name__ == '__main__':
-	s()
+	test_in_python()
