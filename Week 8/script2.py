0ca4424a50a5e2b54099176b268a88a5c31e91a3
hjiang00 <hjiang00@umail.ucsb.edu>
2019-03-02 21:40:04 -0800

    edit output method for test data

--- Week 8/script2.py
+++ Week 8/script2.py
@@ -6,7 +6,8 @@ from memory_profiler import profile
 
 #@profile
 def test_in_python(input):
-	sub_thread_time = subprocess.Popen("/usr/bin/time -lp python fibonacci3.py " + input + " >> output.txt", shell = True, creationflags =0)
+	log = open('output-fib-cpython-terminal.txt', 'a')
+	sub_thread_time = subprocess.Popen("/usr/bin/time -lp python fibonacci3.py " + input, shell = True, creationflags =0, stdout = log, stderr = log)
 	#Mem info
 	memory_useinfo = 0
 	#Basic time
