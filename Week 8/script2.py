f4003431ab15b6fc8aad2963ebaca444cde44017
hjiang00 <hjiang00@umail.ucsb.edu>
2019-03-14 21:32:08 -0700

    final clean up

--- Week 8/script2.py
+++ Week 8/script2.py
@@ -6,7 +6,7 @@ from memory_profiler import profile
 
 #@profile
 def test_in_python(input):
-	log = open('pi-pypy-test.txt', 'a')
+	log = open('pi-pypy-terminal.txt', 'a')
 	sub_thread_time = subprocess.Popen("/usr/bin/time -lp pypy pidigits-jython.py " + input, shell = True, creationflags =0, stdout = log, stderr = log)
 	#sub_thread_time = subprocess.Popen("/usr/bin/time -lp " + input, shell = True, creationflags =0, stdout = log, stderr = log)
 	#Mem info
