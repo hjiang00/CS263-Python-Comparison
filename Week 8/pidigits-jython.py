1b987ecc607fd8e2ffe44a7f417d841281085280
zhichengzhang1995 <zhichengzhang00@umail.ucsb.edu>
2019-03-14 17:18:57 -0700

    Final push

--- Week 8/pidigits-jython.py
+++ Week 8/pidigits-jython.py
@@ -15,24 +15,28 @@ i = k = ns = 0
 k1 = 1
 n,a,d,t,u = (1,0,1,0,0)
 while(1):
-    k += 1
+    k = 1 + k
     t = n<<1
-    n *= k
-    a += t
-    k1 += 2
-    a *= k1
-    d *= k1
+    n = k * n
+    a = t + a
+    k1 = 2 + k1
+    a = k1 * a
+    d = k1 * d
     if a >= n:
         t,u = divmod(n*3 +a,d)
-        u += n
+        u = n + u
         if d > u:
             ns = ns*10 + t
-            i += 1
+            i = 1 + i
             if i % 10 == 0:
                 #print ('%010d\t:%d' % (ns, i))
                 ns = 0
             if i >= N:
                 break
-            a -= d*t
-            a *= 10
-            n *= 10
+            a = a - d*t
+            a = 10 * a
+            n = 10 * n
+k = 0
+k += n
+print "this is n ", k
+print "this is d ", d
--- Week 8/script2.py
+++ Week 8/script2.py
@@ -6,16 +6,16 @@ from memory_profiler import profile
 
 #@profile
 def test_in_python(input):
-	log = open('rg-nuitka-terminal.txt', 'a')
-	#sub_thread_time = subprocess.Popen("/usr/bin/time -lp python l_regex-dna.py " + input, shell = True, creationflags =0, stdout = log, stderr = log)
-	sub_thread_time = subprocess.Popen("/usr/bin/time -lp " + input, shell = True, creationflags =0, stdout = log, stderr = log)
+	log = open('pi-pypy-test.txt', 'a')
+	sub_thread_time = subprocess.Popen("/usr/bin/time -lp pypy pidigits-jython.py " + input, shell = True, creationflags =0, stdout = log, stderr = log)
+	#sub_thread_time = subprocess.Popen("/usr/bin/time -lp " + input, shell = True, creationflags =0, stdout = log, stderr = log)
 	#Mem info
 	memory_useinfo = 0
 	#Basic time
 	start_time = time.time()
 	#Thread1:
-	#sub_thread = subprocess.Popen("python l_regex-dna.py " + input, shell = True, creationflags = 0)
-	sub_thread = subprocess.Popen('' + input, shell = True, creationflags = 0)
+	sub_thread = subprocess.Popen("pypy pidigits-jython.py " + input, shell = True, creationflags = 0)
+	#sub_thread = subprocess.Popen('' + input, shell = True, creationflags = 0)
 	#Log cpu_time, memory_useinfo
 	while sub_thread.poll() is None:
 		try:
@@ -39,12 +39,19 @@ def test_in_python(input):
 
 
 if __name__ == '__main__':
-	test_in_python("./l_regex-dna.bin 7 0 < regexredux-input.txt")
-	test_in_python("./l_regex-dna.bin 8 0 < regexredux-input.txt")
-	test_in_python("./l_regex-dna.bin 9 0 < regexredux-input.txt")
-	test_in_python("./l_regex-dna.bin 10 0 < regexredux-input.txt")
-	test_in_python("./l_regex-dna.bin 11 0 < regexredux-input.txt")
-	test_in_python("./l_regex-dna.bin 12 0 < regexredux-input.txt")
+	# test_in_python("./l_regex-dna.bin 7 0 < regexredux-input.txt")
+	# test_in_python("./l_regex-dna.bin 8 0 < regexredux-input.txt")
+	# test_in_python("./l_regex-dna.bin 9 0 < regexredux-input.txt")
+	# test_in_python("./l_regex-dna.bin 10 0 < regexredux-input.txt")
+	# test_in_python("./l_regex-dna.bin 11 0 < regexredux-input.txt")
+	# test_in_python("./l_regex-dna.bin 12 0 < regexredux-input.txt")
+	test_in_python("10000")
+	test_in_python("15000")
+	test_in_python("20000")
+	test_in_python("25000")
+	test_in_python("30000")
+	test_in_python("35000")
+
 
 	#test_in_python("./thread-ring.bin 1000000")
 	# test_in_python("./fibonacci3.bin 10")
